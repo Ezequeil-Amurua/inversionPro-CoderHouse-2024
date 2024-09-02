790164960232e68a3910cd3d3a6681a5907915e5
Ezequiel Amurua <ezequielamurua@hotmail.com>
2024-09-02 00:31:54 -0300

    js, function sugerirPortafolio + function comenzarSimulador, + confirm

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ let capitalDisponible;
 let plazoInversion;
 
 const portafolios = [
-    { nombre: "Conservador", descripcion: "Inversiones en Bonos de bajo riesgo.", riesgo: "Bajo" },
-    { nombre: "Moderado", descripcion: "Inversiones en Acciones y bonos", riesgo: "Medio"},
-    { nombre: "Agresivo", descripcion: "Inversiones en Acciones y Criptomonedas", riesgo:"Alto"},
+    { nombre: "Conservador.", descripcion: "Inversiones en Bonos de bajo riesgo.", riesgo: "Bajo." },
+    { nombre: "Moderado.", descripcion: "Inversiones en Acciones y bonos.", riesgo: "Medio."},
+    { nombre: "Agresivo.", descripcion: "Inversiones en Acciones y Criptomonedas.", riesgo:"Alto."},
 ]
 
 function saludarUsuario() {
@@ -16,10 +16,9 @@ function saludarUsuario() {
         }
     } while (nombreUsuario == null || nombreUsuario == "" || !isNaN(nombreUsuario));
     
-    alert("Hola " + nombreUsuario.toUpperCase() + "." + "\nA continuación vamos a planificar tu Inversión");
+    alert("Hola " + nombreUsuario.toUpperCase() + "." + "\nA continuación vamos a planificar tu Inversión.");
 }
 
-saludarUsuario();
 
 function obtenerInfoUsuario () {
     const invertisteAlgunaVez = confirm("¿Alguna vez a has hecho inversiones?");
@@ -31,7 +30,7 @@ function obtenerInfoUsuario () {
     }
 
     do{
-        capitalDisponible = parseFloat(prompt("¿Cuánto capital estas dispuesto a invertir? , recuerda indicar el monto en USD."));
+        capitalDisponible = parseFloat(prompt("¿Cuánto capital estas dispuesto a invertir?. Recuerda indicar el monto en USD."));
         if(isNaN(capitalDisponible) || capitalDisponible <= 0) {
             alert ("Por favor, ingresá una cantidad válida.");
         } 
@@ -46,4 +45,35 @@ function obtenerInfoUsuario () {
     }while (isNaN(plazoInversion) || plazoInversion <= 0);
 }
 
-obtenerInfoUsuario ()
+
+function sugerirPortafolio () {
+    let portafolioSugerido;
+
+    if (plazoInversion <= 6){
+        portafolioSugerido = portafolios[0];
+    }else if (plazoInversion <= 18) {
+        portafolioSugerido = portafolios [1];
+    }else {
+        portafolioSugerido = portafolios[2];
+    }
+
+alert ("Basandonos en tu plazo de inversión, te sugerimos el siguiente Portafolio de Inversión :\n\n" + portafolioSugerido.nombre + "\n" + portafolioSugerido.descripcion + "\nRiesgo : " + portafolioSugerido.riesgo)
+}
+
+
+function comenzarSimulador() {
+    saludarUsuario();
+    obtenerInfoUsuario ();
+    sugerirPortafolio ();
+
+    const confirmacion = confirm ("¿Quieres confirmar la inversión en el Portafolio sugerido?");
+
+    if (confirmacion) {
+        alert("Perfecto, tu inversión ha sido solicitada con éxito!")
+    }else {
+        alert("No hay problema, puedes volver a comenzar el simulador cuando desees.")
+        comenzarSimulador ();
+    }
+}
+
+comenzarSimulador();

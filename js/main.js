c4554e780561c82e0ab8b8b9e701fe42caaeab53
Ezequiel Amurua <ezequielamurua@hotmail.com>
2024-10-17 02:15:36 -0300

    html, css, js, json, readme

--- js/main.js
+++ js/main.js
@@ -1,109 +1,333 @@
-/*Cursos - Carrito */
+//DOM
+const sectionCursos = document.getElementById("section-cursos");
+const verCarrito = document.getElementById("ver-carrito");
+const modalContainer = document.getElementById("modal-container")
 
-const shopCursos = document.getElementById("shop-cursos");
-const verCarrito = document.getElementById("verCarrito");
-const modalContainer = document.getElementById("modal-container")     
-
-const cursos = [
-    { id: 1, nombre: "Finanzas Personales", precio: 20000 },
-    { id: 2, nombre: "Blockchain y Criptomonedas", precio: 20000 },
-    { id: 3, nombre: "Invertir en Bolsa", precio: 20000 },
-    { id: 4, nombre: "Analisis Tecnico", precio: 20000 },
-];
+const dolarHoy = document.getElementById("dolar-hoy")
+const sectionCotizaciones = document.getElementById("section-cotizaciones")
+const sectionCriptos = document.getElementById("section-criptos")
 
 
+//Array Carrito 
 let carrito = [];
 
-
+//Traer el carrito de LocalStorage
 const carritoStorage = JSON.parse(localStorage.getItem("carrito"));
 if (carritoStorage) {
     carrito = carritoStorage;
 }
 
 
+function guardarCarrito() {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
 
-cursos.forEach((curso) =>{
-    let contenido = document.createElement("div");
-    contenido.className = "card";
 
-    contenido.innerHTML = `
-    <h3>  ${curso.nombre}</h3>
-    <p> <span class = "signo">$</span> ${curso.precio}</p>
-    `;
+//Fetch
+fetch("./cursos.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error en la Red");
+        }
+        return response.json();
+        console.log(cursos)
+    })
+    .then(cursos => {
+        // Cursos Cards
+        cursos.forEach(curso => {
+            let contenedor = document.createElement("div");
+            contenedor.className = "container-curso";
 
-    shopCursos.appendChild(contenido);
+            contenedor.innerHTML = `<h2 class ="color"> ${curso.nombre}</h2>
+                                        <h3> $${curso.precio} <span class="off">${curso.promocion}</span></h3>
+                                        <h4 class = "color">⏱${curso.duracion}</h4>`;
 
-    let comprar = document.createElement("button")
-    comprar.innerText = "Comprar";
+            sectionCursos.appendChild(contenedor);
 
-    contenido.appendChild(comprar)
+            // Creando Botón Comprar
+            let comprarCurso = document.createElement("button");
+            comprarCurso.innerText = "Comprar";
+            comprarCurso.className = "btn-comprar";
 
-    comprar.addEventListener("click", () =>{
-        carrito.push({
-            id : curso.id,
-            nombre : curso.nombre,
-            precio : curso.precio,
+            contenedor.appendChild(comprarCurso);
+
+            // Agregar Curso al carrito 
+            comprarCurso.addEventListener("click", () => {
+
+                //Validacion si ya esta agregado un curso al carrito
+                const cursoExistente = carrito.some(cursoEnCarrito => cursoEnCarrito.nombre === curso.nombre);
+                if (cursoExistente) {
+                    Toastify({
+                        text: `El curso ${curso.nombre} ya está en el carrito.`,
+                        duration: 2000,
+                        gravity: "top",
+                        position: "left",
+                        stopOnFocus: true,
+                        style: {
+                            background: "red",
+                            color: "white",
+                        },
+                    }).showToast();
+                } else {
+                    carrito.push({
+                        nombre: curso.nombre,
+                        precio: curso.precio,
+                    });
+                    Toastify({
+                        text: `Agregaste ${curso.nombre}`,
+                        duration: 2000,
+                        gravity: "top",
+                        position: "left",
+                        stopOnFocus: true,
+                        style: {
+                            background: "linear-gradient(135deg, #efb42c, #e4d8a3, #fdd15b)",
+                            color: "black",
+                        },
+                    }).showToast();
+                    guardarCarrito();
+                }
+            });
         });
-
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-        console.log(carrito);
-
-    }) 
-});
-
-
-verCarrito.addEventListener("click", () => {
-
-    modalContainer.innerHTML = "";
-    modalContainer.style.display = "flex";
-    const headerModal = document.createElement("div");
-    headerModal.className = "header-modal"
-    headerModal.innerHTML = `
-    <h1 class= "modal-header-title">Compras </h1>
-    `;
-
-        modalContainer.append(headerModal);
-    
-
-    const modalBtn = document.createElement("h1");
-    modalBtn.innerText = "X";
-    modalBtn.className = "modal-btn"
-
-    modalBtn.addEventListener("click", () => {
-        modalContainer.style.display = "none";
+    })
+    .catch(error => {
+        console.error("Hubo un problema con la solicitud fetch:", error);
     });
 
-        headerModal.append(modalBtn);
+
+//Carrito
+verCarrito.addEventListener("click", () => {
+
+    modalContainer.classList.add("active");
+
+    const cabeceraModal = document.createElement("div");
+    cabeceraModal.className = "cabecera-modal"
+    cabeceraModal.innerHTML = `<h2 class = "cabecera-title">Tus productos</h2>`;
+
+    modalContainer.appendChild(cabeceraModal);
+
+    //Cerrar Modal
+    const cerrarModal = document.createElement("button");
+    cerrarModal.innerText = "X";
+    cerrarModal.className = "btn-black";
+
+    cerrarModal.addEventListener("click", () => {
+        modalContainer.classList.remove("active");
+        modalContainer.innerHTML = '';
+    });
+
+    cabeceraModal.append(cerrarModal);
+
+    if (carrito.length === 0) {
+        const contenidoCarrito = document.createElement("div");
+        contenidoCarrito.className = "contenido-carrito";
+        contenidoCarrito.innerHTML = `<h2 class="color">Tu carrito está vacío</h2>`;
+        modalContainer.append(contenidoCarrito);
+        return;
+    }
+
+    //Cursos en el carrito
+    carrito.forEach((curso, index) => {
+        let contenidoCarrito = document.createElement("div");
+        contenidoCarrito.className = "contenido-carrito";
+        contenidoCarrito.innerHTML = `<h2 class="color">${curso.nombre}</h2>
+                                        <h3>$${curso.precio}</h3>`;
+
+        //X para borrar curso
+        const btnEliminar = document.createElement("p");
+        btnEliminar.innerText = "X";
+        btnEliminar.className = "p-light";
 
 
+        btnEliminar.addEventListener("click", () => {
+            carrito.splice(index, 1);
+            modalContainer.innerHTML = "";
+            verCarrito.click();
+            guardarCarrito();
+            Toastify({
+                text: `Haz quitado el curso ${curso.nombre}`,
+                duration: 2000,
+                gravity: "top",
+                position: "left",
+                stopOnFocus: true,
+                style: {
+                    background: "linear-gradient(135deg, #efb42c, #e4d8a3, #fdd15b)",
+                    color: "black",
+                },
+            }).showToast();
+        });
 
-    carrito.forEach((curso) => {
-        let carritoContenido = document.createElement("div");
-        carritoContenido.className = "contenido-modal";
-        carritoContenido.innerHTML = `
-        <h3>${curso.nombre}</h3>
-        <p> ${curso.precio}</p>    
-        `;  
+        contenidoCarrito.appendChild(btnEliminar);
+        modalContainer.append(contenidoCarrito);
+    });
 
-        modalContainer.append(carritoContenido);
-    })
-
-
+    //Total Compra
     const total = carrito.reduce((acc, e) => acc + e.precio, 0);
 
-    const totalCompra = document.createElement("div")
+    const totalCompra = document.createElement("div");
     totalCompra.className = "total-compra"
-    totalCompra.innerHTML = `<h2> Total de la compra: $ ${total}</h2>`;
-    
+    totalCompra.innerHTML = `<h2> Total : $${total}</h2>`;
 
-        modalContainer.append(totalCompra);
+    modalContainer.append(totalCompra);
+
+    //BTN para pagar
+    const btnPagar = document.createElement("button");
+    btnPagar.className = "btn-black"
+    btnPagar.innerText = "Pagar"
+
+    totalCompra.append(btnPagar)
+
+    btnPagar.addEventListener("click", () => {
+        modalContainer.innerHTML = "";
+        //Creando Formulario de Pago
+        const formularioPago = document.createElement("div");
+        formularioPago.className = "formulario-pago";
+
+
+        formularioPago.innerHTML = `
+            <div class = " cabecera-modal-form">
+                <h3 class = "formulario-title">Total a pagar $ ${total}</h3>
+                <button  class = "btn-black" id = "cerrarFormulario">    X  </button>
+            </div>
+
+            <div class = "contenido-carrito-form">   
+                <label for="nombre">Nombre:</label>
+                <input type="text" id="nombre" required value ="Javier Gimenez">
+
+                <label for="email">Email:</label>
+                <input type="email" id="email" required value ="elpeladejavasscript@gmail.com">
+
+                <label for="formaPago">Forma de Pago:</label>
+
+                <select id="formaPago" required>
+                    <option value="tarjeta">Tarjeta de Crédito o Débito</option>
+                    <option value="transferencia">Transferencia Bancaria</option>
+                </select>
+            </div>
+            
+
+            <div class ="formulario-pago">
+
+                <label for="numeroTarjeta">Número de Tarjeta:</label>
+                <input type="text" id="numeroTarjeta" required value="4517 5678 9012 3456">
+                
+                <label for="fechaExpiracion">Fecha de Expiración (MM/AA):</label>
+                <input type="text" id="fechaExpiracion" class = "pago" placeholder="MM/AA" required value="12/25">
+
+                <label for="codigoSeguridad">Código de Seguridad:</label>
+                <input type="text" id="codigoSeguridad" class = "pago" required value="383">
+                
+            </div>
+
+            <button type="submit" class="btn-black">Confirmar Pago</button>`;
+
+        modalContainer.appendChild(formularioPago);
+
+        //Cerrar Formulario
+        const cerrarFormulario = document.getElementById("cerrarFormulario");
+        cerrarFormulario.addEventListener("click", () => {
+
+            modalContainer.classList.remove("active");
+            modalContainer.innerHTML = " ";
+        })
+
+        // Manejar el envío del formulario
+        formularioPago.querySelector("button[type='submit']").addEventListener("click", (event) => {
+            event.preventDefault();
+
+
+            // Manejo de datos del formulario
+            const nombre = document.getElementById("nombre").value;
+
+            // Mostrar un mensaje de confirmación
+            modalContainer.classList.remove("active");
+            modalContainer.innerHTML = "";
+            Swal.fire({
+                icon: "success",
+                width: "490px",
+                position: "bottom-end",
+                title: "¡Bienvenido a Inversion PRO!",
+                text: `Te enviamos el link de acceso a la plataforma al email ingresado`,
+                showConfirmButton: true,
+                confirmButtonColor: "green",
+                timerProgressBar: "true",
+                timer: 9500
+            });
+            ;
+        })
+
+        // Limpiamos Carrito
+        carrito = [];
+        guardarCarrito();
+
+    });
 
 });
 
 
+//API COTIZACIONES Bluelytics 
+fetch("https://api.bluelytics.com.ar/v2/latest")
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+
+        const contentDolar = document.createElement("nav");
+        contentDolar.innerHTML = `
+                                <ul class="navbar-cotizaciones">
+                                    <li><a id="dolar-hoy"> Dolar BLUE $${data.blue.value_sell}</a></li>
+                                    
+                                    <li><a href="#ethereum">Ethereum $ 2.434 (ETH)</a></li>
+                                    <li><a href="#oro">ORO $ 2.704</a></li>
+                                    <li><a href="#euro">(EURO) $${data.oficial_euro.value_sell} </a></li>
+                                </ul>`;
+
+        sectionCotizaciones.appendChild(contentDolar);
+    }).catch((error) => {
+        const errorCotizaciones = document.getElementById(errorCotizaciones);
+        errorCotizaciones.textContent = `Cargando Cotizaciones...`
+    });
 
 
 
+//API Cotizaciones CoinGecko
+const traerCriptos = async () => {
+    const errorCriptos = document.getElementById("errorCriptos");
+    errorCriptos.innerHTML = "";
+
+    try {
+        const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=8&page=1");
+        const data = await response.json();
+        console.log(data)
+
+
+        const contenidoCoins = document.createElement("nav");
+        contenidoCoins.className = "navbar-criptos"
+        const lista = document.createElement("ul");
+
+        data.forEach(cripto => {
+            const item = document.createElement("li");
+
+            const img = document.createElement("img");
+            img.src = cripto.image;
+            img.alt = cripto.name;
+            img.className = "img-cripto"
+
+            item.appendChild(img)
+
+            const link = document.createElement("a");
+            link.textContent = `${cripto.name} $${cripto.current_price}`
+
+            item.appendChild(link)
+            lista.appendChild(item);
+        });
+
+        contenidoCoins.appendChild(lista);
+        sectionCriptos.appendChild(contenidoCoins);
+
+    } catch (error) {
+        errorCriptos.textContent = `Cargando Criptomonedas...`;
+    }
+}
+
+traerCriptos();
 
 
 //section test inversor//
@@ -113,85 +337,3 @@ const sectionDelTest = document.getElementById("section-test");
 btnTest.addEventListener("click", () => {
     sectionDelTest.classList.toggle("hidden");
 });
-
-
-
-// let nombreUsuario;
-// let capitalDisponible;
-// let plazoInversion;
-
-// const portafolios = [
-//     { nombre: "Conservador.", descripcion: "Inversiones en Bonos de bajo riesgo.", riesgo: "Bajo." },
-//     { nombre: "Moderado.", descripcion: "Inversiones en Acciones y bonos.", riesgo: "Medio."},
-//     { nombre: "Agresivo.", descripcion: "Inversiones en Acciones y Criptomonedas.", riesgo:"Alto."},
-// ]
-
-// function saludarUsuario() {
-//     do {
-//         nombreUsuario = prompt("Bienvenido a Inversión Pro, ¿cuál es tu nombre?");
-//         if (nombreUsuario == null || nombreUsuario == "" || !isNaN(nombreUsuario)) {
-//             alert("Por favor, ingrese un nombre válido.");
-//         }
-//     } while (nombreUsuario == null || nombreUsuario == "" || !isNaN(nombreUsuario));
-    
-//     alert("Hola " + nombreUsuario.toUpperCase() + "." + "\nA continuación vamos a planificar tu Inversión.");
-// } 
-
-
-// function obtenerInfoUsuario () {
-//     const invertisteAlgunaVez = confirm("¿Alguna vez a has hecho inversiones?");
-
-//     if (invertisteAlgunaVez) {
-//         alert ("Genial, vamos a personalizar tu próxima inversion!")
-//     }else {
-//         alert("No te preocupes, te ayudaremos a comenzar")
-//     }
-
-//     do{
-//         capitalDisponible = parseFloat(prompt("¿Cuánto capital estas dispuesto a invertir?. Recuerda indicar el monto en USD."));
-//         if(isNaN(capitalDisponible) || capitalDisponible <= 0) {
-//             alert ("Por favor, ingresá una cantidad válida.");
-//         } 
-//     }while (isNaN(capitalDisponible) || capitalDisponible <= 0);
-
-//     do {
-//         plazoInversion = parseInt(prompt("¿Cuánto tiempo estimás en meses, que estarías dispuesto a Invertir tus " + capitalDisponible + "USD?")); {
-//         if (isNaN(plazoInversion) || plazoInversion <= 0) {
-//             alert("Por favor, ingresá una cantidad de meses válida.")
-//         }
-//         }
-//     }while (isNaN(plazoInversion) || plazoInversion <= 0);
-// }
-
-
-// function sugerirPortafolio () {
-//     let portafolioSugerido;
-
-//     if (plazoInversion <= 6){
-//         portafolioSugerido = portafolios[0];
-//     }else if (plazoInversion <= 18) {
-//         portafolioSugerido = portafolios [1];
-//     }else {
-//         portafolioSugerido = portafolios[2];
-//     }
-
-// alert ("Basandonos en tu plazo de inversión, te sugerimos el siguiente Portafolio de Inversión :\n\n" + portafolioSugerido.nombre + "\n" + portafolioSugerido.descripcion + "\nRiesgo : " + portafolioSugerido.riesgo)
-// }
-
-
-// function comenzarSimulador() {
-//     saludarUsuario();
-//     obtenerInfoUsuario ();
-//     sugerirPortafolio ();
-
-//     const confirmacion = confirm ("¿Quieres confirmar la inversión en el Portafolio sugerido?");
-
-//     if (confirmacion) {
-//         alert("Perfecto, tu inversión ha sido solicitada con éxito!")
-//     }else {
-//         alert("No hay problema, puedes volver a comenzar el simulador cuando desees.")
-//         comenzarSimulador ();
-//     }
-// }
-
-// comenzarSimulador();
